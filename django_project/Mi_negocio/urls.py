1cae1adcba4c2c3fc613772e3323771387373e3f
carlosr301101 <carlosr301101@gmail.com>
2025-01-17 00:17:43 -0500

    using layouts for display the views

--- django_project/Mi_negocio/urls.py
+++ django_project/Mi_negocio/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path('subir/', views.subir_articulo, name='subir_articulo'),
     path('articulos/', views.lista_articulos, name='lista_articulos'),
 ]
