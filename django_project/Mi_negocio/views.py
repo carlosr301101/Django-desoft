3984b59a728bb95df966a4566454113c525e88fd
carlosr301101 <carlosr301101@gmail.com>
2025-01-16 21:49:54 -0500

    adding features

--- django_project/Mi_negocio/views.py
+++ django_project/Mi_negocio/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Mi primera app")
+    return render(request,"Mi_negocio/index.html")
+
+def greet(request, name:str):
+    return render(request, "Mi_negocio/greet.html",{
+     "name": name.capitalize()   
+    })
