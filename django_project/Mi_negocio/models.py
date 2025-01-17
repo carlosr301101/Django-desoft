243d1bac1f889c744a3e5fb38cdf2de5ccbea28e
carlosr301101 <carlosr301101@gmail.com>
2025-01-16 22:09:42 -0500

    adding new features for images

--- django_project/Mi_negocio/models.py
+++ django_project/Mi_negocio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.TextField(verbose_name="Introducir el precio")
+    imagen = models.ImageField(upload_to='articulos/')
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
--- django_project/Mi_negocio/views.py
+++ django_project/Mi_negocio/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ArticuloForm
+from .models import Articulo
 # Create your views here.
 def index(request):
     return render(request,"Mi_negocio/index.html")
@@ -9,3 +10,18 @@ def greet(request, name:str):
     return render(request, "Mi_negocio/greet.html",{
      "name": name.capitalize()   
     })
+
+
+def subir_articulo(request):
+    if request.method == 'POST':
+        form = ArticuloForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_articulos')
+    else:
+        form = ArticuloForm()
+    return render(request, 'mi_app/subir_articulo.html', {'form': form})
+
+def lista_articulos(request):
+    articulos = Articulo.objects.all()
+    return render(request, 'mi_app/lista_articulos.html', {'articulos': articulos})
