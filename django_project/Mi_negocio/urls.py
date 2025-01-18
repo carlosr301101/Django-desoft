af81ae53739c8f549cc3ba0a66c3081b1cc962ed
carlosr301101 <carlosr301101@gmail.com>
2025-01-17 19:49:10 -0500

    login funcional

--- django_project/Mi_negocio/urls.py
+++ django_project/Mi_negocio/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
-    path("", views.blank, name="index"),
+   # path("", views.blank, name="index"),
+    
+    #path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.index, name='index'),
     path('subir/', views.subir_articulo, name='subir_articulo'),
     path('articulos/', views.lista_articulos, name='lista_articulos'),
 ]
--- django_project/Mi_negocio/views.py
+++ django_project/Mi_negocio/views.py
@@ -1,9 +1,24 @@
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ArticuloForm
 from .models import Articulo
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
+
+
+def custom_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')  # Redirige a la página de inicio
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'Mi_negocio/login.html', {'form': form})
 
 def blank (request):
     return HttpResponseRedirect('subir')
