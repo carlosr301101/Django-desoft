d6b5b65a13c2c2556a3fac4459354551cace3899
carlosr301101 <carlosr301101@gmail.com>
2025-01-29 16:52:13 -0500

    article forms pretty

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5$j9p_(u3j#=y6&h+*f^^@-aa6lkd-zb9e&@3k)b7)=5zgy-&#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
--- django_project/django_project/wsgi.py
+++ django_project/django_project/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+path=os.path.expanduser('~/Minegocio')
+if path not in sys.path:
+    sys.path.insert(0,path)
+    os.environ['DJANGO_SETTINGS_MODULE']='django_project.setting'
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
